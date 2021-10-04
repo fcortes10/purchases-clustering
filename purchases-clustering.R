65fb2ebcd24a548e9fcbbe02f0fc1bb8c10e6d4a
Cortes Tejada <fcortes@intercorp.com.pe>
2021-10-03 19:48:57 -0500

    database cleaning and reading

--- purchases-clustering.R
+++ purchases-clustering.R
@@ -1,2 +1,15 @@
 library(data.table)
+library(plyr)
 
+dt <- fread('data-csv/purchase-card-transactions-201612.csv', nrows = 0)
+
+files <- list.files('data-csv', pattern = '..csv', full.names = TRUE)
+
+column.names <- toupper(gsub(" ", "_", colnames(dt)))
+
+list.dt <- lapply(files, fread, select = colnames(dt), col.names = column.names)
+
+dt <- as.data.table(ldply(list.dt, data.frame))
+
+rm(list.dt, files, column.names)
+gc()
